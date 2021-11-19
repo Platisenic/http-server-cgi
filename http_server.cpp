91aa3d0fd5d79aacbb8bde025edf7daa7790b1f9
libos <benson60703@gmail.com>
2021-11-20 02:12:26 +0800

    fix: Change directory

--- http_server.cpp
+++ http_server.cpp
@@ -1,13 +1,3 @@
-//
-// async_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
 #include <cstdlib>
 #include <iostream>
 #include <memory>
@@ -16,10 +6,10 @@
 
 using boost::asio::ip::tcp;
 
-class session : public std::enable_shared_from_this<session>
+class http_connection : public std::enable_shared_from_this<http_connection>
 {
 public:
-  session(tcp::socket socket)
+  http_connection(tcp::socket socket)
     : socket_(std::move(socket)) {}
 
   void start(){
@@ -68,13 +58,13 @@ private:
   tcp::socket socket_;
   enum { max_length = 1024 };
   char data_[max_length];
-  char exec_pannel[200] = "./cgi/panel.cgi";
+  char exec_pannel[200] = "./panel.cgi";
   char success_status[200] = "HTTP/1.1 200 OK\r\n";
 };
 
-class server{
+class http_server{
 public:
-  server(boost::asio::io_context& io_context, short port)
+  http_server(boost::asio::io_context& io_context, short port)
     : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)){
     do_accept();
   }
@@ -84,7 +74,7 @@ private:
     acceptor_.async_accept(
         [this](boost::system::error_code ec, tcp::socket socket){
           if (!ec){
-            std::make_shared<session>(std::move(socket))->start();
+            std::make_shared<http_connection>(std::move(socket))->start();
           }
           do_accept();
         });
@@ -93,16 +83,15 @@ private:
   tcp::acceptor acceptor_;
 };
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
   try{
     if (argc != 2){
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+      std::cerr << "Usage: http_server <port>\n";
       return 1;
     }
 
     boost::asio::io_context io_context;
-    server s(io_context, std::atoi(argv[1]));
+    http_server s(io_context, std::atoi(argv[1]));
     io_context.run();
   }catch (std::exception& e){
     std::cerr << "Exception: " << e.what() << "\n";
